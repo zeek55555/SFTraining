b753896cd60078e48ff223b038cab967282ccd08
Warren Barnes <wbarnes@apterainc.com>
2017-10-25 15:44:42 -0400

    Add Complimentor widget

---
+++ SitefinityTraining/Mvc/Controllers/ComplimentsController.cs
@@ -0,0 +1,23 @@
+﻿using SitefinityWebApp.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "Complimentor", Title = "Complimentor", SectionName = "Whatever")]
+    public class ComplimentsController : Controller
+    {
+        public string Compliments { get; set; }
+
+        public ActionResult Index()
+        {
+            //ViewBag.MyCompliments = Compliments;
+            var model = new ComplimentsModel(Compliments);
+            return View(model);
+        }
+    }
+}
---
+++ SitefinityTraining/Mvc/Models/ComplimentsModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitefinityWebApp.Mvc.Models
+{
+    public class ComplimentsModel
+    {
+        private string _compliments;
+
+        public string Compliments {
+            get {
+                return _compliments;
+            }
+            set
+            {
+                _compliments = value;
+            }
+        }
+        
+        private string[] _complimentsArray => _compliments.Split(',').Select(_compliments => _compliments.Trim()).ToArray();
+
+        public ComplimentsModel()
+        {
+            _compliments = "You are as cool as you think";
+        }
+        public ComplimentsModel(string compliments)
+        {
+            this._compliments = compliments;
+        }
+        public string ComplimentMe()
+        {
+            Random rnd = new Random();
+            int r = rnd.Next(_complimentsArray.Count());
+            return _complimentsArray[r];
+        }
+    }
+}
