920a50e85099b996293b0b4717b5b8f0d5035f67
Warren Barnes <wbarnes@apterainc.com>
2017-10-30 14:56:24 -0400

    Add some tests around ComplimentsModel

---
+++ SitefinityWebApp.Tests/Compliments/TheComplimentsModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SitefinityWebApp.Mvc.Controllers;
+using SitefinityWebApp.Mvc.Models;
+
+namespace SitefinityWebApp.Tests.Compliments
+{
+    [TestClass]
+    public class TheComplimentsModel
+    {
+        [TestMethod]
+        public void Exists()
+        {
+            var complimentsModel= new ComplimentsModel();
+        }
+
+        [TestMethod]
+        public void HasComplimentras()
+        {
+            var complimentsModel = new ComplimentsModel();
+            Assert.AreEqual("You ARE as cool as you think", complimentsModel.Compliments);
+        }
+
+        [TestMethod]
+        public void CanCompliemnt()
+        {
+            var complimentsModel = new ComplimentsModel();
+            Assert.AreEqual("You ARE as cool as you think", complimentsModel.ComplimentMe());
+        }
+
+        [TestClass]
+        public class GivenSomeCompliments
+        {
+            [TestMethod]
+            public void ItWillComplimentYouWithVariety()
+            {
+
+            }
+        }
+    }
+}
---
+++ SitefinityWebApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SitefinityWebApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SitefinityWebApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5cf65d89-ca17-4102-9b18-32348bc673f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
